893ce2c7c3de71051eaf440ed13674d64aaab0b9
James Butcher <jamesbutcher@duck.com>
2024-03-14 22:31:09 +0000

    Evolution of algae

--- src/lsystem/core.clj
+++ src/lsystem/core.clj
@@ -1,7 +1,21 @@
 (ns lsystem.core
   (:gen-class))
 
+(def rules
+  {'A '(A B A)
+   'B '(A A B)})
+
+(defn- progress
+  [rules expr]
+  (reduce (fn [acc x] (concat acc (get rules x))) nil expr))
+
+(defn run
+  [rules axiom n & {:keys [x] :or {x 0}}]
+  (if (< x n)
+    (run rules (progress rules axiom) n :x (+ x 1))
+    axiom))
+  
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run rules '(A B A) 5))
